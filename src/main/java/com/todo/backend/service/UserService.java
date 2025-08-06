2f1f32c483b450e9533f17fed1b41c3c12daad67
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-06 15:26:33 +0900

    JPA 연동

---
+++ src/main/java/com/todo/backend/service/UserService.java
@@ -0,0 +1,23 @@
+package com.todo.backend.service;
+
+import com.todo.backend.entity.User;
+import com.todo.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
