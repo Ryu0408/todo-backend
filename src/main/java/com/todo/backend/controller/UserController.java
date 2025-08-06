ff644db9240068d5794afc6d181a1ff639bea00a
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-06 15:26:33 +0900

    JPA 연동

---
+++ src/main/java/com/todo/backend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.todo.backend.controller;
+
+import com.todo.backend.entity.User;
+import com.todo.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        return userService.findByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
