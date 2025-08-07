93037422952b6fab7be981def5a95e59c4067081
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-07 13:27:32 +0900

    파일 업로드 테스트

--- src/main/java/com/todo/backend/service/S3Service.java
+++ src/main/java/com/todo/backend/service/S3Service.java
@@ -3,9 +3,17 @@ package com.todo.backend.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class S3Service {
@@ -22,13 +30,35 @@ public class S3Service {
     @Value("${aws.s3.bucketName}")
     private String bucketName;
 
-    // 파일 업로드 메서드
+    private final S3Client s3Client;
+
+    public S3Service() {
+        // AWS SDK 클라이언트 설정
+        s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+                .build();
+    }
+
     public String uploadFile(MultipartFile file) throws IOException {
-        // 업로드 로직 (AWS S3 관련)
-        // 예시로만 작성된 로직입니다.
-        File tempFile = new File(file.getOriginalFilename());
-        file.transferTo(tempFile);
-        // AWS S3 업로드 로직 구현...
-        return "File uploaded to S3 successfully!";
+        // 파일을 임시로 저장
+        Path tempFilePath = Paths.get(System.getProperty("java.io.tmpdir"), file.getOriginalFilename());
+        file.transferTo(tempFilePath.toFile());
+
+        // S3에 업로드할 객체 생성
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(file.getOriginalFilename())
+                .contentType(file.getContentType())
+                .build();
+
+        // S3에 파일 업로드
+        s3Client.putObject(putObjectRequest, tempFilePath);
+
+        // 업로드 완료 후 임시 파일 삭제
+        Files.delete(tempFilePath);
+
+        // S3에 업로드된 파일 URL 반환 (여기선 URL 생성 방법은 예시로 설정)
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + file.getOriginalFilename();
     }
 }
