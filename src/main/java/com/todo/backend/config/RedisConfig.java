d8355782c0772b06fea682ebd8b1463606f7d905
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-08 10:39:00 +0900

    REDIS 에러 예외처리 확인

--- src/main/java/com/todo/backend/config/RedisConfig.java
+++ src/main/java/com/todo/backend/config/RedisConfig.java
@@ -3,12 +3,14 @@ package com.todo.backend.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 @EnableCaching
@@ -23,5 +25,25 @@ public class RedisConfig {
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    // 🔧 Redis 장애 시 500 안 나게(예외 삼키고 경고 로그만)
+    @Bean
+    public CacheErrorHandler cacheErrorHandler() {
+        Logger log = LoggerFactory.getLogger("CacheErrorHandler");
+        return new SimpleCacheErrorHandler() {
+            @Override public void handleCacheGetError(RuntimeException ex, org.springframework.cache.Cache cache, Object key) {
+                log.warn("Redis GET 실패 - cache={}, key={}, err={}", cache.getName(), key, ex.toString());
+            }
+            @Override public void handleCachePutError(RuntimeException ex, org.springframework.cache.Cache cache, Object key, Object value) {
+                log.warn("Redis PUT 실패 - cache={}, key={}, err={}", cache.getName(), key, ex.toString());
+            }
+            @Override public void handleCacheEvictError(RuntimeException ex, org.springframework.cache.Cache cache, Object key) {
+                log.warn("Redis EVICT 실패 - cache={}, key={}, err={}", cache.getName(), key, ex.toString());
+            }
+            @Override public void handleCacheClearError(RuntimeException ex, org.springframework.cache.Cache cache) {
+                log.warn("Redis CLEAR 실패 - cache={}, err={}", cache.getName(), ex.toString());
+            }
+        };
     }
 }
