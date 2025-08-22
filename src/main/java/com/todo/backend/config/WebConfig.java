d5d3ff7364a76709c0484e2f17a833d0bfc26d70
Seunghwan Ryu <57667994+Ryu0408@users.noreply.github.com>
2025-08-22 10:38:19 +0900

    수정

--- src/main/java/com/todo/backend/config/WebConfig.java
+++ src/main/java/com/todo/backend/config/WebConfig.java
@@ -9,9 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("https://ryu-todo.com") // 요청 허용할 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드
-                .allowedHeaders("*"); // 모든 헤더 허용
+        registry.addMapping("/**")
+                .allowedOrigins("https://ryu-todo.com", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키나 Authorization 헤더 필요 시
     }
 }
